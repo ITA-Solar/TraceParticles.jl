e73fbcdf973b6dd57d98cd4cb7fe1999f6be10c6
eilifso <e.s.oyre@astro.uio.no>
2024-04-17 15:09:57 +0200

    [DEV] Destagger bifrost-gradients. Allow for cubic spline by creating uniform axes.

--- src/io/bifrost_input.jl
+++ src/io/bifrost_input.jl
@@ -17,7 +17,7 @@ function get_br_var_interpolator(
         expdir  ::String,
         variable::String,
         ;
-        itp_type=Gridded(Linear()),
+        itp_type=BSpline(Linear()),
         itp_bc=Flat(),
         kwargs...
     )
@@ -25,18 +25,44 @@ function get_br_var_interpolator(
     var = dropdims(var)
     br_axes = load_br_axes(expname, snap, expdir)
     br_axes = dropdims(br_axes)
-    var_itp = linear_interpolation(
-        br_axes, var, extrapolation_bc=itp_bc
-        )
-    #var_itp = interpolate(br_axes, var, itp_type)
-    #var_itp = extrapolate(var_itp, itp_bc)
-    return var_itp
+        #var_itp = linear_interpolation(
+    #    br_axes, var, extrapolation_bc=itp_bc
+    #    )
+    if itp_type == BSpline(Linear())
+        itp_func = linear_interpolation
+    else
+        range_axes = Vector{StepRangeLen}(undef, length(br_axes))
+        for i in eachindex(br_axes)
+            gridspacing = diff(br_axes[i])
+            if all(gridspacing[1] .== gridspacing)
+                range_axes[i] = range(br_axes[i][1], br_axes[i][end], length(br_axes[i]))
+            end
+        end
+        mask = [isassigned(range_axes,i) for i in eachindex(range_axes)]
+        if all(mask)
+            br_axes = Tuple(range_axes)
+        else
+            @warn "Non-uniform grid. Creating uniform axes " *
+                "using range."
+            idx = findall(x->!x, mask)
+            for i in idx
+                range_axes[i] = range(start=br_axes[i][1], stop=br_axes[i][end], length=length(br_axes[i]))
+            end
+            br_axes = Tuple(range_axes)
+        end
+        itp_func = cubic_spline_interpolation
+        #var_itp = interpolate(var, itp_type)
+        #var_itp = extrapolate(scale(var_itp, br_axes...), itp_bc)
+    end
+
+    var_itp = itp_func(br_axes, var, extrapolation_bc = itp_bc)
+    return var_itp, br_axes
 end
 
 
 function get_br_emfield_vecof_interpolators(args...; kwargs...)
     vars = ("bx", "by", "bz", "ex", "ey", "ez")
-    return [get_br_var_interpolator(args..., var; kwargs...) for var in vars]
+    return [get_br_var_interpolator(args..., var; kwargs...)[1] for var in vars]
 end
 
 
@@ -46,39 +72,128 @@ function get_br_gcafields_vecof_interpolators(
         expdir  ::String,
         ;
         itp_bc=Flat(),
+        itp_type=Gridded(Linear()),
+        destagger=true,
+        destaggergradients=true,
+        diffscheme=bifrostscheme,
         kwargs...
         )
     vars = ("bx", "by", "bz", "ex", "ey", "ez")
-    emfields = [get_var(expname, snap, expdir, var; kwargs...)
-        for var in vars]
-    B_vec = stack(emfields[1:3])
-    E = stack(emfields[4:6])
+    emfields = [get_var(expname, snap, expdir, var;
+        destagger=destagger, kwargs...)
+        for var in vars] # Vector{Array{3, <:Real}}
+    B_vec = stack(emfields[1:3]) # Array{4, <:Real}
+    E_vec = stack(emfields[4:6]) # Array{4, <:Real}
+
+    println("Avg E: $(sum(E_vec)/prod(size(E_vec)))")
+    println("Scaling:")
+    #B_vec *= 1.121e3*1e-4 # u_B*cgs2SI_b
+    #E *= 1e6*1.121e3*1e-6 # u_u*u_B*cgs2SI_E
+    println("Avg E after scaling: $(sum(E_vec)/prod(size(E_vec)))")
+
+
+    # br_axes = Tuple{Vector{<:Real}, Vector{<:Real}, Vector{<:Real}}
     br_axes = load_br_axes(expname, snap, expdir)
+
     println("Computing gradients...")
+    # ∇B = Vector{Array{3, <:Real}}(3)
+    # ∇b = ∇ExB = Vector{Array{3, <:Real}}(9)
     ∇B, ∇b̂, ∇ExBdrift = tp.gcagradients_from_emfield(
         B_vec,
-        E,
+        E_vec,
         br_axes...,
-        derivateupwind
+        diffscheme,
         )
 
     # Drop extra dimensions
-    B_vec = dropdims(B_vec)
-    E = dropdims(E)
-    ∇B = dropdims.(∇B)
-    ∇b̂ = dropdims.(∇b̂)
-    ∇ExBdrift = dropdims.(∇ExBdrift)
-    br_axes = dropdims(br_axes)
+    B_vec = dropdims(B_vec) # Array{2, <:Real}
+    E_vec = dropdims(E_vec)         # Array{2, <:Real}
+    ∇B = dropdims.(∇B)      # Vector{Array{2, <:Real}}(3)
+    ∇b̂ = dropdims.(∇b̂)      # Vector{Array{2, <:Real}}(9)
+    ∇ExBdrift = dropdims.(∇ExBdrift) # Vector{Array{2, <:Real}}(9)
+    br_axes = dropdims(br_axes) # Tuple{Vector{<:Real}, Vector{<:Real}
 
-    itps = [linear_interpolation(br_axes, var, extrapolation_bc=itp_bc)
-        for var in (
-            [B_vec[:,:,i] for i in 1:3]...,
-            [E[:,:,i] for i in 1:3]...,
-            ∇B...,
-            ∇b̂...,
-            ∇ExBdrift...
-            )
-        ]
+    if itp_type == BSpline(Linear())
+        itp_func = linear_interpolation
+    elseif itp_type == BSpline(Cubic())
+        range_axes = Vector{StepRangeLen}(undef, length(br_axes))
+        for i in eachindex(br_axes)
+            gridspacing = diff(br_axes[i])
+            if all(gridspacing[1] .== gridspacing)
+                range_axes[i] = range(br_axes[i][1], br_axes[i][end], length(br_axes[i]))
+            end
+        end
+        mask = [isassigned(range_axes,i) for i in eachindex(range_axes)]
+        if all(mask)
+            br_axes = Tuple(range_axes)
+        else
+            @warn "Non-uniform grid. Creating uniform axes " *
+                "using range."
+            idx = findall(x->!x, mask)
+            for i in idx
+                range_axes[i] = range(start=br_axes[i][1], stop=br_axes[i][end], length=length(br_axes[i]))
+            end
+            br_axes = Tuple(range_axes)
+        end
+        itp_func = cubic_spline_interpolation
+    elseif itp_type == Gridded(Linear())
+        nothing
+    else
+        error("Unsupported interpolation type.")
+    end
+
+    itps = Vector{AbstractInterpolation}(undef, 27)
+    if destaggergradients
+        # E⃗ and B⃗ are cell centred. The gradients have been calculated with
+        # an upwind scheme, so they are on the cell faces, needing different
+        # interpolation axes than E⃗ and B⃗.
+        if length(br_axes) == 2
+            # Ill defined upper boundary
+            xcentre_axis = br_axes[1]
+            zcentre_axis = br_axes[2]
+            if typeof(br_axes[1]) <: StepRangeLen
+                dx = step(br_axes[1])
+                dz = step(br_axes[2])
+                xface_axis = br_axes[1][1] + dx/2:dx:br_axes[1][end] + dx/2
+                zface_axis = br_axes[2][1] + dz/2:dz:br_axes[2][end] + dz/2
+            else
+                xface_axis = br_axes[1] .+ [diff(xcentre_axis)/2; 0.0]
+                zface_axis = br_axes[2] .+ [diff(zcentre_axis)/2; 0.0]
+            end
+        else
+            error("Cell centred gradient interpolation objects unavailable in 3D")
+        end
+        # Create interpolation objects of cell centred components.
+        itps[1:6] = [itp_func(br_axes, var, extrapolation_bc=itp_bc)
+            for var in ([B_vec[:,:,i] for i in 1:3]...,[E_vec[:,:,i] for i in 1:3]...)]
+        # Create interpolation objects of upper x-face components, i.e ∂ₓfᵢ components.
+        itps[[7, 10:12..., 19:21...]] = [itp_func(
+            (xface_axis, zcentre_axis), var, extrapolation_bc=itp_bc)
+            for var in (∇B[1], ∇b̂[1:3]...,∇ExBdrift[1:3]...)]
+        # Create interpolation objects of upper z-face components, i.e ∂_zf_z components.
+        itps[[9, 16:18..., 25:27...]] = [itp_func(
+                (xcentre_axis, zface_axis), var, extrapolation_bc=itp_bc)
+                for var in (∇B[3], ∇b̂[7:9]...,∇ExBdrift[7:9]...)]
+        # zero itp struct for the ∂_zf_z components...
+        # A Functor returning 0.0 that is a subtype og AbstractInterpolation is 3
+        # times faster. A function returning 0.0 is 30 times faster. However, then
+        # itps have to redefined, and maybe become type unstable, which may slow
+        # down the code.
+        itps[[8, 13:15..., 22:24...]] = [itp_func(
+            (0:1, 0:1), zeros(2,2), extrapolation_bc=itp_bc) for i in 1:7]
+        #
+    else
+        itps[:] = [itp_func(br_axes, var, extrapolation_bc=itp_bc)
+            for var in (
+                [B_vec[:,:,i] for i in 1:3]...,
+                [E_vec[:,:,i] for i in 1:3]...,
+                ∇B...,
+                ∇b̂...,
+                ∇ExBdrift...,
+                )
+            ]
+    end
+    return itps
 end
 
 
