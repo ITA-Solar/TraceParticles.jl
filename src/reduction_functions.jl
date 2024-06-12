b050e44b378e1477e035f481a50ffbef91e53a99
eilifso <e.s.oyre@astro.uio.no>
2024-06-12 16:22:56 +0200

    [DEV] Add some summary statistics to reduction function.

--- src/reduction_functions.jl
+++ src/reduction_functions.jl
@@ -11,15 +11,24 @@ end
 function (self::SaveBatchAsDF)(u, batch, I)
     # Save the batch as a DataFrame
     self.batchnr += 1
-    filename = self.expname * "_batch_$(self.batchnr)_size_$(self.batchsize).csv"
+    filename = self.expname * "_$(self.batchnr).csv"
     df = DataFrame(batch)
     filename = joinpath(self.expdir, filename)
-    println("=================================================================")
     println("Saving batch $(self.batchnr) (particles $I) to")
     println("--> $(self.expdir)")
     println("--> $(filename)")
     CSV.write(filename, df)
     println("success")
+    println("")
+    println("Summary statistics:")
+    println("Number of particles:         $(length(batch))")
+    println("Average number of timesteps: $(mean(df.nt))")
+    println("Number of maxiters:          $(sum(df.retcode .== :maxiters))")
+    println("Number of OutofDomain:       $(sum(df.retmsg .== "OutofDomain"))")
+    println("Number of GCABreakDown:      $(sum(df.retmsg .== "GCABreakDown"))")
+    println("")
     println("=================================================================")
+    println("")
+    println("Running batch $(self.batchnr) of $(self.batchsize).")
     return u, false
 end
