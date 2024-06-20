5286e74d47cfb5e5acaf98124a5424fa0836760a
eilifso <e.s.oyre@astro.uio.no>
2024-06-20 13:46:25 +0200

    [DEV][CLN] Add `collect_batches`.

--- src/dataprocessing/dataprocessing.jl
+++ src/dataprocessing/dataprocessing.jl
@@ -143,47 +143,11 @@ function generate_probdistr(
 end
 
 
-function theoretical_energygain_2Dxz(
-    solution::ODESolution
-    )
-    error("Incomplete function. Need a way to calculate the electric potential.")
-    nsteps = length(solution.t)
-    charge = solution.prob.p[1]
-    x0 = solution.u[1][1]
-    z0 = solution.u[1][3]
-    E_0 = norm([solution.prob.p[4][i](x0, z0) for i = 4:6])
-    E_gain = Vector{eltype(E_0)}(undef, nsteps - 1)
-    for i = 1:nsteps-1
-        x = solution.u[i][1]
-        z = solution.u[i][3]
-        E_gain = norm([solution.prob.p[4][i](x, z) for i = 4:6]) - E_0
+function collect_batches(filename::String, nbatches::Int)
+    df = DataFrame(CSV.File(filename * "_1.csv"))
+    for i in 2:nbatches
+        df_tmp = DataFrame(CSV.File(filename * "_$i.csv"))
+        df = vcat(df, df_tmp)
     end
-    return charge*E_gain
-end
-
-function avgnumsteps(
-    solution::Vector{Tuple{
-        Any, Any, Any, Vector, Vector, Vector, Vector, Any, Any
-        }}
-    )
-    sum = 0
-    for particle in solution
-        sum += particle[8]
-    end
-    return sum/length(solution)
-end
-
-
-function findinittemp(
-    solution::Vector{Tuple{
-        Any, Any, Any, Vector, Vector, Vector, Vector, Any, Any
-        }},
-    tempitp::AbstractInterpolation
-    )
-    nofparticles = length(solution)
-    T = Vector{typeof(solution[1][1][1])}(undef, nofparticles)
-    for (particle, i) in zip(solution, 1:nofparticles)
-        T[i] = tempitp(particle[1][1], particle[1][3])
-    end
-    return T
+    return df
 end
