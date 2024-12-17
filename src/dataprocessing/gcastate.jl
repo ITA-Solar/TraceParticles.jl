a15d4a970406c489cd46816e9c7e71ccfcd17938
eilifso <e.s.oyre@astro.uio.no>
2024-12-17 23:27:14 +0100

    [DEV] Format gcastate.jl.

--- src/dataprocessing/gcastate.jl
+++ src/dataprocessing/gcastate.jl
@@ -95,30 +95,32 @@ struct GCAState
     L_B::Real
 
     function GCAState(
-        state ::Vector{<:Real},
-        q     ::Real,
-        m     ::Real,
-        μ     ::Real, 
-        itpvec::Vector{<:AbstractInterpolation}; 
+        state::Vector{<:Real},
+        q::Real,
+        m::Real,
+        μ::Real,
+        itpvec::Vector{<:AbstractInterpolation};
         time=0.0,
         dimensionality="2Dxz"
-        )
+    )
         if dimensionality == "2Dxz"
             R = [state[1], state[3]]
-        else
+        elseif dimensionality == "3D"
             R = state[1:3]
+        else
+            error("Dimensionality not implemented")
         end
         vparal = state[4]
-        q⁻¹ = 1/q # Inverse of q - to replace division with multiplication
+        q⁻¹ = 1 / q # Inverse of q - to replace division with multiplication
         # Use the gyrocentre position interpolate the vectors
         # scalars from the interpolation objects.
         B_vec = [itpvec[i](R...) for i in 1:3]
         E_vec = [itpvec[i](R...) for i in 4:6]
         B = norm(B_vec)   # The magnetic field strength
-        B⁻¹ = 1/B         # Inverse of B - to replace divition with multipl.
-        b̂ = B_vec*B⁻¹     # An unit vector pointing in the direction of the
-        ExBdrift = (E_vec × b̂)/B # The E cross B-drift
-    
+        B⁻¹ = 1 / B         # Inverse of B - to replace divition with multipl.
+        b̂ = B_vec * B⁻¹     # An unit vector pointing in the direction of the
+        ExBdrift = (E_vec × b̂) / B # The E cross B-drift
+
         if length(itpvec) == 27
             ∇B = [itpvec[i](R...) for i in 7:9]
             ∇b̂ = reshape([itpvec[i](R...) for i in 10:18], 3, 3)
@@ -129,26 +131,26 @@ struct GCAState
                 B_vec_fd = vec[1:3]
                 E_vec_fd = vec[4:6]
                 B_fd = norm(B_vec_fd)
-                b̂_fd = B_vec_fd/B_fd
-                return [b̂_fd; E_vec_fd × b̂_fd/B_fd; B_fd]
+                b̂_fd = B_vec_fd / B_fd
+                return [b̂_fd; E_vec_fd × b̂_fd / B_fd; B_fd]
             end
             # Add zeros-column representing derivatives along the y-axis
             if dimensionality == "2Dxz"
                 jacobian_matrix = [
-                    jacobian_matrix[:,1];;
+                    jacobian_matrix[:, 1];;
                     zeros(eltype(R), 7);;
-                    jacobian_matrix[:,2]
-                    ]
+                    jacobian_matrix[:, 2]
+                ]
             end
-            ∇b̂ = jacobian_matrix[1:3,:]
-            ∇ExB = jacobian_matrix[4:6,:]
-            ∇B = jacobian_matrix[7,:]
+            ∇b̂ = jacobian_matrix[1:3, :]
+            ∇ExB = jacobian_matrix[4:6, :]
+            ∇B = jacobian_matrix[7, :]
         end
-    
+
         # Total time derivatives. Assumes ∂/∂t = 0,
-        dbdt = vparal * (∇b̂ * b̂) + ∇b̂*ExBdrift
-        dExBdt = vparal * (∇ExB * b̂) + ∇ExB*ExBdrift
-    
+        dbdt = vparal * (∇b̂ * b̂) + ∇b̂ * ExBdrift
+        dExBdt = vparal * (∇ExB * b̂) + ∇ExB * ExBdrift
+
         # Calculate other drifts
         ∇Bdrift = gradbdrift(b̂, ∇B, μ, B⁻¹, q⁻¹)
         Rdrift = curvaturedrift(b̂, dbdt, vparal, B⁻¹, q⁻¹, m)
@@ -159,15 +161,15 @@ struct GCAState
         dbdtacc = drift_dbdt_acceleration(ExBdrift, ∇Bdrift, dbdt)
         # Compute the perpendicular velocity
         # Other auxiliary quantities
-        vperp = √(2B*μ/m) # The perpendicular velocity
+        vperp = √(2B * μ / m) # The perpendicular velocity
         Ek = kineticenergy(
             vparal, vperp, ExBdrift, ∇Bdrift, Rdrift, Pdrift, m
-            )
+        )
         L_B = characteristicfieldlength(B, ∇B)
         r_L = larmorradius(m, vperp, q, B)
         ω_c = gyrofrequency(m, q, B)
         β = cosineof_pitchangle(B_vec, vparal, m, μ)
-    
+
         return new(
             state,
             q, m, μ, time,
@@ -180,25 +182,28 @@ struct GCAState
         )
     end
 
-   function GCAState(
-        state    ::Vector{<:Real},
-        q        ::Real,
-        m        ::Real,
-        μ        ::Real,
-        bfield   ::Vector{<:Real},
-        efield   ::Vector{<:Real}
+    function GCAState(
+        state::Vector{<:Real},
+        q::Real,
+        m::Real,
+        μ::Real,
+        bfield::Vector{<:Real},
+        efield::Vector{<:Real}
         ;
         time=-1
-        )
+    )
         vparallel = state[4]
         B = norm(bfield)
         v_E = exbdrift(bfield, efield)
         vperp = perpendicular_velocity(μ, m, B)
         Ek = kineticenergy(vparallel, vperp, v_E, m)
         new(
-            state, q, m, μ, time, Ek, vperp, -1,-1, -1, v_E,
-            -1ones(3), -1ones(3), -1ones(3), -1, -1, -1, bfield, efield, -1
-            )
+            state, q, m, μ, time, Ek, vperp, -1, -1, -1, v_E,
+            -1ones(3), -1ones(3), -1ones(3),
+            -1, -1, -1,
+            bfield, efield,
+            -1
+        )
     end
 
     function GCAState(
@@ -207,10 +212,10 @@ struct GCAState
         ;
         lowmem=true,
         kwargs...
-        )
+    )
         return init_and_final_ensembleofgcastates(
             solution, fields; lowmem=lowmem, kwargs...
-            )
+        )
     end
 end
 
@@ -230,7 +235,7 @@ function get_drifts(gcastate::GCAState)
         gcastate.∇Bdrift,
         gcastate.curvaturedrift,
         gcastate.polarisationdrift
-        ]
+    ]
 end
 
 
@@ -285,7 +290,7 @@ function get_fieldlength(gcastates::Vector{GCAState})
     [gcastate.L_B for gcastate in gcastates]
 end
 function get_scalesratio(gcastates::Vector{GCAState})
-    [gcastate.r_L/gcastate.L_B for gcastate in gcastates]
+    [gcastate.r_L / gcastate.L_B for gcastate in gcastates]
 end
 function get_exb(gcastates::Vector{GCAState})
     [norm(gcastate.exbdrift) for gcastate in gcastates]
@@ -327,7 +332,7 @@ function get_driftnorm(gcastates::Vector{GCAState})
         gcastate.∇Bdrift .+
         gcastate.curvaturedrift .+
         gcastate.polarisationdrift
-        ) for gcastate in gcastates]
+    ) for gcastate in gcastates]
 end
 
 
@@ -347,7 +352,7 @@ function get_forces(gcastates::Vector{GCAState})
     times = get_time(gcastates)
     Δdrifts = [diff(drift) for drift in drifts]
     Δt = diff(times)
-    perpendicular_acceleration = [ Δv ./ Δt for Δv in Δdrifts]
+    perpendicular_acceleration = [Δv ./ Δt for Δv in Δdrifts]
 
     # "Gyration force"
     Δvperp = diff(get_vperp(gcastates))
@@ -358,23 +363,23 @@ function get_forces(gcastates::Vector{GCAState})
     ΔEΔt = ΔE ./ Δt
     # Take the pairwise average of the parallel acceleration to get the
     # acceleration at the same time.
-    pairwiseaverage(x) = ((x .+ circshift(x, -1))/2)[1:end-1]
+    pairwiseaverage(x) = ((x.+circshift(x, -1))/2)[1:end-1]
     mirroracc = pairwiseaverage(get_mirroracc(gcastates))
     parallelacc = pairwiseaverage(get_paralacc(gcastates))
     acceleration = [[parallelacc, mirroracc]; perpendicular_acceleration]
-    force = get_mass(gcastates)*acceleration
+    force = get_mass(gcastates) * acceleration
 
     # Return a NamedTuple of the time and force components.
     return (
-        time = pairwiseaverage(times),
-        parallel = force[1],
-        mirror = force[2],
-        exb = force[3],
-        gradb = force[4],
-        curvature = force[5],
-        polarisation = force[6],
-        vperp = force[7],
-        energy = ΔEΔt
+        time=pairwiseaverage(times),
+        parallel=force[1],
+        mirror=force[2],
+        exb=force[3],
+        gradb=force[4],
+        curvature=force[5],
+        polarisation=force[6],
+        vperp=force[7],
+        energy=ΔEΔt
     )
 end
 
@@ -411,7 +416,7 @@ function DataFrame(gcastates::Vector{GCAState}; lowmem=false)
     df = DataFrame(
         Dict(
             field => [getfield(state, field) for state in gcastates]
-                for field in fieldnames(GCAState)
+            for field in fieldnames(GCAState)
         )
     )
     newcols = Dict(
@@ -449,7 +454,8 @@ function DataFrame(gcastates::Vector{GCAState}; lowmem=false)
     end
     # Remove the vector fields
     for source in values(newcols)
-        try select!(df, Not(source[1]))
+        try
+            select!(df, Not(source[1]))
         catch
         end
     end
@@ -457,25 +463,27 @@ function DataFrame(gcastates::Vector{GCAState}; lowmem=false)
     if !lowmem
         select!(df,
             [
-            :Rx, :Ry, :Rz, :vparal, :charge, :mass, :μ, :time, :energy, :vperp,
-            :r_L, :ω_c, :β, :exbdrift_x, :exbdrift_y,
-            :exbdrift_z, :∇Bdrift_x, :∇Bdrift_y, :∇Bdrift_z, :curvaturedrift_x,
-            :curvaturedrift_y, :curvaturedrift_z, :polarisationdrift_x,
-            :polarisationdrift_y, :polarisationdrift_z, :mirroracc, :paralacc,
-            :dbdtacc, :bx, :by, :bz, :ex, :ey, :ez, :L_B
+                :Rx, :Ry, :Rz, :vparal, :charge, :mass, :μ, :time, :energy, :vperp,
+                :r_L, :ω_c, :β, :exbdrift_x, :exbdrift_y,
+                :exbdrift_z, :∇Bdrift_x, :∇Bdrift_y, :∇Bdrift_z, :curvaturedrift_x,
+                :curvaturedrift_y, :curvaturedrift_z, :polarisationdrift_x,
+                :polarisationdrift_y, :polarisationdrift_z, :mirroracc, :paralacc,
+                :dbdtacc, :bx, :by, :bz, :ex, :ey, :ez, :dbdt_x, :dbdt_y,
+                :dbdt_z, :L_B
             ]
-            )
+        )
     else
         select!(df,
             [
-            :energy, :vperp,
-            :r_L, :ω_c, :β, :exbdrift_x, :exbdrift_y,
-            :exbdrift_z, :∇Bdrift_x, :∇Bdrift_y, :∇Bdrift_z, :curvaturedrift_x,
-            :curvaturedrift_y, :curvaturedrift_z, :polarisationdrift_x,
-            :polarisationdrift_y, :polarisationdrift_z, :mirroracc, :paralacc,
-            :dbdtacc, :bx, :by, :bz, :ex, :ey, :ez, :L_B
+                :energy, :vperp,
+                :r_L, :ω_c, :β, :exbdrift_x, :exbdrift_y,
+                :exbdrift_z, :∇Bdrift_x, :∇Bdrift_y, :∇Bdrift_z, :curvaturedrift_x,
+                :curvaturedrift_y, :curvaturedrift_z, :polarisationdrift_x,
+                :polarisationdrift_y, :polarisationdrift_z, :mirroracc, :paralacc,
+                :dbdtacc, :bx, :by, :bz, :ex, :ey, :ez, :dbdt_x, :dbdt_y,
+                :dbdt_z, :L_B
             ]
-            )
+        )
     end
     return df
 end
@@ -506,7 +514,7 @@ function timeseriesofgcastates(
     ;
     components="all",
     dimensionality="2Dxz"
-    )
+)
     timeseries = Vector{GCAState}(undef, length(times))
     Threads.@threads for i in eachindex(times)
         interpolated_solution = solution(times[i], idxs=1:4)
@@ -520,7 +528,7 @@ function timeseriesofgcastates(
                 ;
                 time=times[i],
                 dimensionality=dimensionality
-                )
+            )
         else
             itpvec = solution.prob.fields
             if dimensionality == "2Dxz"
@@ -540,7 +548,7 @@ function timeseriesofgcastates(
                 efield
                 ;
                 time=times[i]
-                )
+            )
         end
     end
     return timeseries
@@ -574,7 +582,7 @@ function init_and_final_ensembleofgcastates(
     itpvec::Vector{<:AbstractInterpolation},
     ;
     kwargs...
-    )
+)
     initialstates = getinitialstate(solution)
     finalstates = getfinalstate(solution)
     initialtimes = getinitialtimes(solution)
@@ -587,7 +595,7 @@ function init_and_final_ensembleofgcastates(
         itpvec
         ;
         kwargs...
-        )
+    )
     finalgcastates = ensembleofgcastates(
         finalstates,
         magneticmoments,
@@ -595,7 +603,7 @@ function init_and_final_ensembleofgcastates(
         itpvec
         ;
         kwargs...
-        )
+    )
     return initialgcastates, finalgcastates
 end
 
@@ -650,10 +658,10 @@ function ensembleofgcastates(
     ;
     lowmem=true,
     kwargs...
-    )
+)
     nrows = size(solution, 1)
     gcastates = ensembleofgcastates(
-        [Vector(solution[i,:]) for i in 1:nrows],
+        [Vector(solution[i, :]) for i in 1:nrows],
         magneticmoments,
         times,
         args...
@@ -674,7 +682,7 @@ function ensembleofgcastates(
     mass=tp.m_e,
     dimensionality="2Dxz",
     lowmem=false,
-    )
+)
     nofparticles = length(solution)
     particlestates = Vector{GCAState}(undef, nofparticles)
     Threads.@threads for i in 1:nofparticles
@@ -687,9 +695,7 @@ function ensembleofgcastates(
                 itpvec
                 ;
                 time=times[i],
-                dimensionality=dimensionality,
-
-                )
+                dimensionality=dimensionality,)
         elseif components == "exb"
             if dimensionality == "2Dxz"
                 Rx = solution[i][1]
@@ -708,20 +714,20 @@ function ensembleofgcastates(
                 efield
                 ;
                 time=times[i]
-                )
+            )
         end
     end
     return particlestates
 end
 
 function ensembleofgcastates(
-    solution::Vector{Tuple{Any, Any, Any, Vector, Vector, Vector, Vector, Any, Any}}
+    solution::Vector{Tuple{Any,Any,Any,Vector,Vector,Vector,Vector,Any,Any}}
     ;
     components="exb",
     charge=tp.e,
     mass=tp.m_e,
     dimensionality="2Dxz"
-    )
+)
     nofparticles = length(solution)
     initialstates = Vector{GCAState}(undef, nofparticles)
     finalstates = Vector{GCAState}(undef, nofparticles)
@@ -736,7 +742,7 @@ function ensembleofgcastates(
                 solution[i][5], # electric field
                 ;
                 time=solution[i][9],
-                )
+            )
             finalstates[i] = GCAState(
                 solution[i],
                 charge,
