4e9b63fa4734a9f97f57d61767b44e5107085d2a
eilifso <e.s.oyre@astro.uio.no>
2025-02-06 15:30:37 +0100

    [DEL][CLN] Remove unused functions in `callbacks.jl`.

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,20 +1,3 @@
-
-abstract type AbstractTPCallback end
-
-struct DiscreteTPCallback{F1,F2} <: AbstractTPCallback
-    condition::F1
-    affect!::F2
-    function DiscreteTPCallback(condition::F1, affect!::F2) where {F1,F2}
-        new{F1,F2}(condition, affect!)
-    end
-end
-function DiscreteTPCallback()
-    condition(x) = false
-    affect! = nothing
-    return DiscreteTPCallback(condition, affect!)
-end
-
-
 """
     callbacks.jl
 
@@ -282,59 +265,4 @@ function larmorradius(
     q = params.charge
     m = params.mass
     r_L = larmorradius(R, μ, q, m, itpvec)
-end
-
-#------------------------------------------------------------------------------
-# MIN callback for characteristic field length of the magnetic field.
-function mincondition_fieldlength_2Dxz(u, _, integrator)
-    itpvec = integrator.p.fields[1:3]
-    L_B = characteristicfieldlength([u[1], u[3]], itpvec)
-    return L_B < integrator.p.fieldlength.min
-end
-function mincondition_fieldlength_highmemory_2Dxz(u, _, integrator)
-    itpvec = integrator.p.fields
-    R = [u[1], u[3]]
-    L_B = characteristicfieldlength(R, itpvec[1:3], itpvec[7:9])
-    return L_B < integrator.p.fieldlength.min
-end
-
-# Affects
-function minaffect_fieldlength_2Dxz!(integrator)
-    itpvec = integrator.p[:fields][1:3]
-    L_B = characteristicfieldlength([integrator.u[1], integrator.u[3]], itpvec)
-    integrator.p.fieldlength.min = L_B
-    integrator.p.fieldlength.min_u = copy(integrator.u)
-    integrator.p.fieldlength.min_t = integrator.t
-end
-function minaffect_fieldlength_highmemory_2Dxz!(integrator)
-    itpvec = integrator.p.fields
-    R = [integrator.u[1], integrator.u[3]]
-    L_B = characteristicfieldlength(R, itpvec[1:3], itpvec[7:9])
-    integrator.p.fieldlength.min = L_B
-    integrator.p.fieldlength.min_u = copy(integrator.u)
-    integrator.p.fieldlength.min_t = integrator.t
-end
-
-#------------------------------------------------------------------------------
-# MAX callback for SCALESRATIO
-function maxcondition_scalesratio_2Dxz(u, t, integrator)
-    ratio = scalesratio_2Dxz(u, t, integrator.p)
-    return ratio > integrator.p.scalesratio.max
-end
-function maxcondition_scalesratio_highmemory_2Dxz(u, t, integrator)
-    ratio = scalesratio_highmem_2Dxz(u, t, integrator.p)
-    return ratio > integrator.p.scalesratio.max
-end
-
-function maxaffect_scalesratio_2Dxz!(integrator)
-    ratio = scalesratio_2Dxz(integrator.u, integrator.t, integrator.p)
-    integrator.p.scalesratio.max = ratio
-    integrator.p.scalesratio.max_u = copy(integrator.u)
-    integrator.p.scalesratio.max_t = integrator.t
-end
-function maxaffect_scalesratio_highmemory_2Dxz!(integrator)
-    ratio = scalesratio_highmem_2Dxz(integrator.u, integrator.t, integrator.p)
-    integrator.p.scalesratio.max = ratio
-    integrator.p.scalesratio.max_u = copy(integrator.u)
-    integrator.p.scalesratio.max_t = integrator.t
 end
