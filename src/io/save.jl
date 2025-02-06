f5615c2fb68ced1b0aec6e766f3680e7e41397d2
eilifso <e.s.oyre@astro.uio.no>
2025-02-06 18:51:51 +0100

    [DEL][CLN] Remove outdated save-functions.

--- src/io/save.jl
+++ src/io/save.jl
@@ -8,112 +8,13 @@
 #-------------------------------------------------------------------------------
 # Contains functions for saving data.
 #-------------------------------------------------------------------------------
-"""
-    save(
-        expname::String,
-        expdir ::String,
-        args...
-        )
-Save the data slurped in `args` with `expname` at `expdir`.
-"""
-function save(
-    expname::String,
-    expdir::String,
-    args...
-)
-    basename = joinpath(expdir, expname)
-    filename = string(basename, ".tp")
-    save(filename, args...)
-end
 
 
 """
-    save(
-        filename::String,
-        solution::Array{<:Real, 3},
-        )
-Save the numerical solution which has the form of a 3D-array. E.g. the solution
-to the Loretnz equation.
-"""
-function save(
-    filename::String,
-    solution::Array{<:Real,3},
-)
-    f = open(filename, "w+")
-    write(f, solution)
-    close(f)
-    println("tp.jl: Wrote $filename")
-end
-
-
-"""
-    save(
-        filename::String,
-        solution::Array{<:Real, 3},
-        magneticmoment::Vector{<:Real}
-        )
-Save the numerical solution of the guiding centre approximation - which has the
-form of a 3D-array - and the magnetic moment - which is a parameter of the
-equations.
-"""
-function save(
-    filename::String,
-    solution::Array{<:Real,3},
-    magneticmoment::Vector{<:Real}
-)
-    f = open(filename, "w+")
-    write(f, solution)
-    write(f, magneticmoment)
-    close(f)
-    println("tp.jl: Wrote $filename")
-end
-
-
-"""
-    save_lightweight(
-        filename::String,
-        solutionvector::Vector{<:ODESolution},
-        )
-Save only the initial state, final state, and final time of the ODE-solutions.
-"""
-function save_lightweight(
-    filename::String,
-    solutionvector::Vector{<:ODESolution},
-)
-    u0 = getinitialstate(solutionvector)
-    uf = getfinalstate(solutionvector)
-    tf = getfinaltime(solutionvector)
-    ndof, npart = size(u0)
-    file = open(filename, "w+")
-    write(file, ndof)
-    write(file, npart)
-    write(file, u0)
-    write(file, uf)
-    write(file, tf)
-    close(file)
-    println("tp.jl: Wrote $filename")
-end
-
-
-"""
-    save_gcastates(expdir, expname, fileext, nbatches, fieldname)
     save_gcastates(filename, nbatches, fields_itp)
 Calculates the GCAStates of the initial and final states of an ensamble of
 test particles.
 """
-function save_gcastates(
-    expdir::String,
-    expname::String,
-    fileext::String,
-    nbatches::Int,
-    fieldname::String,
-)
-    @load fieldname fields_itp
-    filename = joinpath(expdir, expname) * fileext
-    save_gcastates(filename, nbatches, fields_itp)
-end
-
-
 function save_gcastates(
     filename::String,
     fields_itp::Vector{<:AbstractInterpolation}
