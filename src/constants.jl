cdbb78962e6bb874decab7019cdf9ff7fb8c5f61
eilifso <e.s.oyre@astro.uio.no>
2025-06-19 22:04:12 +0200

    SYN BUG spaces between exponent.

--- src/constants.jl
+++ src/constants.jl
@@ -13,10 +13,10 @@
 k_B = 1.380649e-23 # J/K..................... Boltzmann's constant
 e = 1.602176634e-19 # C....................... Elementary charge
 c = 299792458 # m/s.................... Speed of light in vacuum
-G = 6.67430e − 11 # m^3 kg^-1 s^-2........ Constant of gravitation
-h = 6.62607015e − 34 # J Hz^-1.................. Planck's constant
-μ_0 = 1.25663706212e − 6 # N A^-2.......Vacuum magnetic permeability
-ϵ_0 = 8.8541878128e − 12 # F m^-1.......Vacuum electric permittivity
+G = 6.67430e-11 # m^3 kg^-1 s^-2........ Constant of gravitation
+h = 6.62607015e-34 # J Hz^-1.................. Planck's constant
+μ_0 = 1.25663706212e-6 # N A^-2.......Vacuum magnetic permeability
+ϵ_0 = 8.8541878128e-12 # F m^-1.......Vacuum electric permittivity
 m_e = 9.1093837015e-31 # Kg ........................ Electron mass
 m_p = 1.67262192369e-27 # Kg ......................... Proton mass
 csqrd = c^2 # s^2 m^-2 ........ Inverse of the light speed squared
